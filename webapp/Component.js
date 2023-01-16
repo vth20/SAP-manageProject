9d56d2b632d2c9ba31d34a6846bfed660be519f1
Huỳnh Văn Thạnh <thanhhv@bap.jp>
2023-01-16 09:38:35 +0000

    init main

---
+++ webapp/Component.js
@@ -0,0 +1,36 @@
+/**
+ * eslint-disable @sap/ui5-jsdocs/no-jsdoc
+ */
+
+sap.ui.define([
+    "sap/ui/core/UIComponent",
+    "sap/ui/Device",
+    "manageproduct/model/models",
+    "sap/ui/model/odata/v2/ODataModel",
+],
+    function (UIComponent, Device, models, ODataModel) {
+        "use strict";
+
+        return UIComponent.extend("manageproduct.Component", {
+            metadata: {
+                manifest: "json"
+            },
+
+            /**
+             * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+             * @public
+             * @override
+             */
+            init: function () {
+                // call the base component's init function
+                UIComponent.prototype.init.apply(this, arguments);
+
+                // enable routing
+                this.getRouter().initialize();
+
+                // set the device model
+                this.setModel(models.createDeviceModel(), "device");
+            }
+        });
+    }
+);
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"manageproduct/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
